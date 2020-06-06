c4a17d5a9de7d6952ca1ba6aa8b7d7c2858ae99f
YY Qian <yy.qian@autodesk.com>
2020-06-06 13:24:59 -0700

    join(long millisecond)

--- src/main/java/com/yiyang/concurrency/five/Main.java
+++ src/main/java/com/yiyang/concurrency/five/Main.java
@@ -3,6 +3,13 @@ package com.yiyang.concurrency.five;
 /**
  * join(): It will put the current thread on wait until the thread on which it is called is dead.
  * If thread is interrupted then it will throw InterruptedException.
+ *
+ * join (long milliseconds)
+ * In the first version of the join() method, instead of indefinitely waiting for the finalization of the thread called,
+ * the calling thread waits for the milliseconds specified as the parameter of the method.
+ * For example, if the object thread1 has thread2.join(1000), thread1 suspends its execution until one of these two conditions are met:
+ *  thread2 has finished its execution
+ *  1,000 milliseconds have passed
  */
 public class Main {
     public static void main(String[] args) {
